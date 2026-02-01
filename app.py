c46db184d7f0f51827e7f00b7eca01d60d299c86
Ravi <rravikumar3095@gmail.com>
2026-02-01 12:29:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,redirect,request,session
 from con_sql import con_to_sql
-
+import os
 
 app = Flask(__name__)
 app.secret_key="dhawal123"
@@ -56,6 +56,8 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port)
+
 
 
