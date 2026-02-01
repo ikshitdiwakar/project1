103b84e4fe0020ee231602749ed9c616aa68c477
Ravi <rravikumar3095@gmail.com>
2026-02-01 15:21:32 +0530

    Update con_sql.py

--- con_sql.py
+++ con_sql.py
@@ -3,8 +3,9 @@ import os
 
 def con_to_sql():
     return mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME")
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT", 39009))
     )
