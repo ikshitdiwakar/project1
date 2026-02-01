eafcfe0a85930c4bca1448c65c452489b2d20c4d
Ravi <rravikumar3095@gmail.com>
2026-02-01 15:58:32 +0530

    Update con_sql.py

--- con_sql.py
+++ con_sql.py
@@ -3,7 +3,11 @@ import os
 from urllib.parse import urlparse
 
 def con_to_sql():
-    url = urlparse(os.environ.get("MYSQL_PUBLIC_URL"))
+    db_url = os.environ.get("MYSQL_PUBLIC_URL")
+    if not db_url:
+        raise Exception("MYSQL_PUBLIC_URL not set")
+
+    url = urlparse(db_url)
 
     return mysql.connector.connect(
         host=url.hostname,
