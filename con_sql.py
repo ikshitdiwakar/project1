34c1916c4d53f23ff855776f36b02d6c1598b2c2
ikshitdiwakar <ikshitdiwakar89@gmail.com>
2026-02-02 09:01:21 +0530

    Update con_sql.py

--- con_sql.py
+++ con_sql.py
@@ -1,18 +1,10 @@
-import mysql.connector
+import psycopg2
 import os
-from urllib.parse import urlparse
 
 def con_to_sql():
-    db_url = os.environ.get("MYSQL_PUBLIC_URL")
-    if not db_url:
-        raise Exception("MYSQL_PUBLIC_URL not set")
-
-    url = urlparse(db_url)
-
-    return mysql.connector.connect(
-        host=url.hostname,
-        user=url.username,
-        password=url.password,
-        database=url.path.lstrip('/'),
-        port=url.port
+    return psycopg2.connect(
+        os.getenv("DATABASE_URL"),
+        sslmode="require"
     )
+
+
